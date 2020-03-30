79ae5f4553eacbf136498363dda91f9a64143410
Jake Wija <jakewija@icloud.com[core]>
2020-03-30 08:08:14 -0700

    OUTLINE function, define test

---
+++ divideTwoIntegers/divideTwoIntegers.js
@@ -0,0 +1,57 @@
+/**
+ * https://leetcode.com/problems/divide-two-integers/
+ * 
+ * Given two integers dividend and divisor, divide two integers without using multiplication, division and mod operator.
+
+Return the quotient after dividing dividend by divisor.
+
+The integer division should truncate toward zero, which means losing its fractional part. For example, truncate(8.345) = 8 and truncate(-2.7335) = -2.
+
+Example 1:
+
+Input: dividend = 10, divisor = 3
+Output: 3
+Explanation: 10/3 = truncate(3.33333..) = 3.
+Example 2:
+
+Input: dividend = 7, divisor = -3
+Output: -2
+Explanation: 7/-3 = truncate(-2.33333..) = -2.
+Note:
+
+Both dividend and divisor will be 32-bit signed integers.
+The divisor will never be 0.
+Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range: [−231,  231 − 1]. For the purpose of this problem, assume that your function returns 231 − 1 when the division result overflows.
+ */
+
+/**
+ * @param {number} dividend
+ * @param {number} divisor
+ * @return {number}
+ */
+
+var divide = function(dividend, divisor) {
+  if (divisor === 0) return undefined;
+  if (dividend === 0) return 0;
+  const isNegative = (dividend < 0 && divisor > 0) || (dividend > 0 && divisor < 0);
+  let numerator = Math.abs(dividend);
+  let denominator = Math.abs(divisor);
+  let count = -1;
+  if (numerator < denominator) {
+    return 0
+  }
+  if (numerator === denominator) {
+    if (isNegative) return -1;
+    return 1;
+  }
+  while (numerator > 0) {
+    numerator -= denominator;
+    count += 1;
+  }
+  if (isNegative) return -count
+  return count;
+};
+
+console.log(divide(-7, 3))
+
+module.exports = divide;
---
+++ divideTwoIntegers/divideTwoIntegers.test.js
@@ -0,0 +1,82 @@
+const divide = require('./divideTwoIntegers');
+
+describe('inputs both positive or negative', ()=>{
+  describe('dividend is larger than divisor', () => {
+    test('correctly divides: 10, 3', () => {
+      expect(divide(10, 3)).toBe(3)
+    })
+  
+    test('correctly divides: -10, -3', () => {
+      expect(divide(-10, -3)).toBe(3)
+    })
+
+    test('correctly divides: -10, -10', () => {
+      expect(divide(-10, -10)).toBe(1)
+    })
+
+    test('correctly divides: 10, 10', () => {
+      expect(divide( 10, 10)).toBe(1)
+    })
+    
+    test('the absolute values of the divisor and dividend are equal and both are equally-signed', () => {
+      expect(divide(1, 1)).toBe(1)
+    })
+
+    test('the absolute values of the divisor and dividend are equal and both are equally-signed', () => {
+      expect(divide(-1, -1)).toBe(1)
+    })
+  })
+  
+  describe('dividend is smaller than divisor, equally-signed', () => {
+    test('returns 0 if dividend is smaller than divisor', () => {
+      expect(divide(3, 6)).toBe(0)
+    })
+
+    test('returns 0 if dividend is smaller than divisor', () => {
+      expect(divide(-7, -3)).toBe(2)
+    })
+  })
+})
+
+describe('one of two inputs is negative', () => {
+  describe('dividend is larger than divisor', () => {
+    test('returns -1 if dividend and divisor are equal and are not equally-signed', () => {
+      expect(divide(-1, 1)).toBe(-1)
+    })
+    
+    test('returns -1 if dividend and divisor are equal and are not equally-signed', () => {
+      expect(divide(1, -1)).toBe(-1)
+    })
+  })
+
+  describe('dividend is smaller than divisor', () => {
+    test('input -7, 3', () => {
+      expect(divide(-7, 3)).toBe(-2)
+    })
+
+    test('input 7, -3', () => {
+      expect(divide(7, -3)).toBe(-2)
+    })
+
+    test('input 3, -7', () => {
+      expect(divide(3, -7)).toBe(0)
+    })
+
+    test('input -3, 7', () => {
+      expect(divide(-3, 7)).toBe(0)
+    })
+  })
+})
+
+describe('edge cases of zero', () => {
+  test('returns undefined for division by zero', () => {
+    expect(divide(9999, 0)).toBeUndefined()
+  })
+
+  test('returns zero when dividend is zero', () => {
+    expect(divide(0, 9999)).toBe(0);
+  })
+})
+
+
+
