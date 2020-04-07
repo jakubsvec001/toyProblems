a75b591e9663529b1f25d8a8ef20785283b39561
Jake Wija <jakewija@icloud.com[core]>
2020-04-06 20:00:18 -0700

    ADD edge case test for bad parameter input

--- removeNthFromEnd/removeNthFromEnd.test.js
+++ removeNthFromEnd/removeNthFromEnd.test.js
@@ -2,6 +2,29 @@ const { removeNthFromEnd, LinkedList } = require('./removeNthFromEnd');
 
 console.log(LinkedList);
 
+test('handles falsy input for first parameter', () => {
+  const m0 = new LinkedList(0);
+  const m1 = m0.add(1);
+  const m2 = m1.add(2);
+  const m3 = m2.add(3);
+  const m4 = m3.add(4);
+  const m5 = m4.add(5);
+  removeNthFromEnd(m0, 0)
+  expect([0,1,2,3,4]).toEqual(m0.toArray())
+});
+
+test('handles falsy input for second parameter', () => {
+  const m0 = new LinkedList(0);
+  const m1 = m0.add(1);
+  const m2 = m1.add(2);
+  const m3 = m2.add(3);
+  const m4 = m3.add(4);
+  const m5 = m4.add(5);
+  removeNthFromEnd(m0, 0)
+  expect([0,1,2,3,4]).toEqual(m0.toArray())
+});
+
+
 test('removes last node', () => {
   const m0 = new LinkedList(0);
   const m1 = m0.add(1);
