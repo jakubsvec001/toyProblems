6a9ae3338305ed773766084facd0312733d90618
Jake Wija <jakewija@icloud.com[core]>
2020-04-08 10:19:02 -0700

    REFACTOR insertionSort and BST with tests

--- balanceBST/balanceBST.js
+++ balanceBST/balanceBST.js
@@ -2,34 +2,26 @@ const insertionSort = (arr) => {
   let sorted = [arr[0]];
   let unsorted = arr.slice(1);
   let toSort;
-  let toCompareUpper;
-  let toCompareLower;
+  let toCompare;
   while (unsorted.length !== 0) {
     toSort = unsorted[0];
     unsorted = unsorted.slice(1);
-    for (
-      let i = sorted.length - 1, j = sorted.length - 2;
-      i >= 0;
-      i -= 1, j -= 1
-    ) {
-      toCompareUpper = sorted[i];
-      toCompareLower = sorted[j];
-      if (toCompareLower === undefined) {
-        if (toSort < toCompareUpper) sorted.unshift(toSort);
-        else sorted.splice(1, 0, toSort);
-        break;
-      } else {
-        if (toSort < toCompareUpper && toSort > toCompareLower) {
-          sorted.splice(i, 0, toSort)
-          break;
+    for (let j = sorted.length - 1; j >= 0; j -= 1) {
+      toCompare = sorted[j];
+      if (j === 0) {
+        if (toSort < sorted[0]){
+          sorted.unshift(toSort)
+        } else {
+          sorted.splice(1, 0, toSort)
         }
-      }
+      } else if (toSort > toCompare) {
+        sorted.splice(j+1, 0, toSort)
+        break
+      } 
     }
   }
   return sorted;
 };
-
-console.log(insertionSort([9, 2, 6, 8, 7, 5, 4, 3, 1]));
 
 const BST = function (val) {
   this.value = val;
@@ -69,30 +61,38 @@ BST.prototype.toArray = function (isDepthFirst = true) {
 
 BST.prototype.add = function (val) {
   let context = this;
-  while (true) {
-    const directions = ['left', 'right'];
-    const direction = val < context.value ? directions[0] : directions[1];
-    if (context[direction] === null) {
-      context[direction] = new BST(val);
-      break;
+  while(true){
+    if (val < context.value) {
+      if (context.left === null) {
+        context.left = new BST(val);
+        break;
+      } else {
+        context = context.left;
+      }
+    } else {
+      if (context.right === null) {
+        context.right = new BST(val);
+        break;
+      } else {
+        context = context.right
+      }
     }
-    context = context[direction];
   }
 };
 
-// root = new BST(3)
-// root.add(5)
-// root.add(1)
-// root.add(3)
-// root.add(2)
-// root.add(4)
-// root.add(7)
-// root.add(8)
-// root.add(9)
-// root.add(10)
-// root.add(11)
-// root.toArray(false)
 
-// BST.prototype.balance = function () {};
+BST.prototype.balance = function () {};
+
+const root = new BST(5)
+root.add(1)
+root.add(3)
+root.add(2)
+root.add(4)
+root.add(10)
+root.add(8)
+root.add(7)
+root.add(9)
+root.add(6)
+console.log(root.toArray(false))
 
 module.exports = { BST, insertionSort };
--- balanceBST/balanceBST.test.js
+++ balanceBST/balanceBST.test.js
@@ -1,40 +1,50 @@
 const { BST, insertionSort } = require('./balanceBST');
 
-let root;
 
 describe('insertionSort inserting properly', () => {
-  test.only('mostly sorted, single insert', () => {
-    expect(insertionSort([1,2,3,4,6,5,7,8,9])).toEqual([1,2,3,4,5,6,7,8,9])
+  
+  test('correctly inserts at beginning ', ()=>{
+    expect(insertionSort([3,2,1])).toEqual([1,2,3])
   })
 
+  test('correctly inserts at the end of sorted array and breaks', ()=>{
+    expect(insertionSort([3,2,1,4])).toEqual([1,2,3,4])
+  })
+
+  test('mostly sorted, single insert', () => {
+    expect(insertionSort([1,2,3,4,6,5,7,8,9])).toEqual([1,2,3,4,5,6,7,8,9])
+  })
+  
   test('unsorted, multiple insert', () => {
     expect(insertionSort([2,9,6,8,7,5,4,3,1])).toEqual([1,2,3,4,5,6,7,8,9])
   })
 })
 
-xdescribe('basic BST methods', () => {
+let root;
+
+describe('basic BST methods', () => {
+  
 
   beforeEach(() => {
-    root = new BST(3)
-    root.add(5)
+    root = new BST(5)
     root.add(1)
     root.add(3)
     root.add(2)
     root.add(4)
-    root.add(7)
-    root.add(8)
-    root.add(9)
     root.add(10)
-    root.add(11)
+    root.add(8)
+    root.add(7)
+    root.add(9)
+    root.add(6)
     return root
   })
 
-  test('Breath first results', () => {
-    expect(root.toArray()).toEqual([3,1,2,5,3,4,7,8,9,10,11])
+  test('Depth first results', () => {
+    expect(root.toArray(true)).toEqual([5,1,3,2,4,10,8,7,6,9])
   })
 
-  test('Depth first results', () => {
-    expect(root.toArray(false)).toEqual([3,1,5,2,3,7,4,8,9,10,11])
+  test('Bredth first results', () => {
+    expect(root.toArray(false)).toEqual([5,1,10,3,8,2,4,7,9,6])
   })
 });
 
