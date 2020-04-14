6d5a3d8ee2b8f329f977157a44bbde34280e73b1
Jake Wija <jakewija@icloud.com[core]>
2020-04-14 09:57:50 -0700

    CLEAN COMMIT

--- algoExpert/easy/twoNumberSum/twoNumberSum.test.js
+++ algoExpert/easy/twoNumberSum/twoNumberSum.test.js
@@ -1,9 +1,20 @@
-const { twoNumberSum }  = require('./twoNumberSum');
+const { twoNumberSum } = require('./twoNumberSum');
 
 describe('base cases', () => {
   test('#1', () => {
-    expect(twoNumberSum([3,5,-4,8,11,1,-1,6], 10).sort()).toEqual([-1, 11])
-  })
+    expect(twoNumberSum([3, 5, -4, 8, 11, 1, -1, 6], 10).sort()).toEqual([
+      -1,
+      11,
+    ]);
+  });
 
-  test.todo('add more tests')
-})
+  test('#2', () => {
+    expect(twoNumberSum([1, 2, 3, 4, 5], 10)).toEqual([]);
+  });
+});
+
+describe('edge cases', () => {
+  test('empty array input', () => {
+    expect(twoNumberSum([])).toEqual([]);
+  });
+});
