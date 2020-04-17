24979cf181bc381370ec9ebf5e417d1ac98069d4
Jake Wija <jakewija@icloud.com[core]>
2020-04-17 13:35:33 -0700

    OUTLINE CODE with comments

--- algoExpert/easy/twoNumberSum/twoNumberSum.js
+++ algoExpert/easy/twoNumberSum/twoNumberSum.js
@@ -4,17 +4,27 @@
 const twoNumberSum = (inputArray, targetSum) => {
   const dir = {};
   let diffTarget;
+  // fill directory with key = number and value = [indexes]
   inputArray.forEach((item, i) => {
     if (dir[item]) dir[item].push(i);
     else dir[item] = [i];
   })
+  // iterate through array again, find diffTarget in directory
   for (let i = 0; i <= inputArray.length - 1; i += 1 ) {
     // calculate difference target
+
     // if diffTarget in directory, check for same index 
+
       // if the directory contains only one index entry for the diffTarget, and our current index is not the same as the index entry: 
-        // return [currentIndex, targetDiff]
+
+        // return [inputArray[i], targetDiff]
+
       // else if directory contains more than one index entry:
+
         // return [currentIndex, targetDiff]
+
+      } 
+    }
   }
   return []
 }
