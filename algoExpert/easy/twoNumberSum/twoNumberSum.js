f7a7e924399f8831b25874841212c227db65e7f6
Jake Wija <jakewija@icloud.com[core]>
2020-04-17 13:23:18 -0700

    CLEAN COMMIT, clean working directory

--- algoExpert/easy/twoNumberSum/twoNumberSum.js
+++ algoExpert/easy/twoNumberSum/twoNumberSum.js
@@ -1,16 +1,33 @@
 /**
  * 
  */
-const twoNumberSum = (array, targetSum) => {
+const twoNumberSum = (inputArray, targetSum) => {
   const dir = {};
-  let difference;
-  array.forEach((item) => {
-    dir[item] = item;
+  let diffTarget;
+  inputArray.forEach((item, i) => {
+    if (dir[item]) dir[item].push(i);
+    else dir[item] = [i];
   })
- 
+  for (let i = 0; i <= inputArray.length - 1; i += 1 ) {
+    // calculate difference target
+    // if diffTarget in directory, check for same index 
+      // if the directory contains only one index entry for the diffTarget, and our current index is not the same as the index entry: 
+        // return [currentIndex, targetDiff]
+      // else if directory contains more than one index entry:
+        // return [currentIndex, targetDiff]
+  }
   return []
 }
 
+// { 
+//   '1': [ 5 ],
+//   '3': [ 0 ],
+//   '5': [ 1, 8 ],
+//   '6': [ 7 ],
+//   '8': [ 3 ],
+//   '11': [ 4 ],
+//   '-4': [ 2 ],
+// } target 10 -> return -1, 11
 
 console.log(twoNumberSum([3, 5, -4, 8, 11, 1, -1, 6], 10))
 
