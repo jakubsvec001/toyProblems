a2f1d1bd248a22d4bc76d762cf7785185568c872
Jake Wija <jakewija@icloud.com[core]>
2020-04-19 14:35:19 -0700

    FINISH implementation of findThreeLargestNumbers with o(n) time

--- aExpert/easy/findThreeLargestNumbers/findThreeLargestNumbers.js
+++ aExpert/easy/findThreeLargestNumbers/findThreeLargestNumbers.js
@@ -6,28 +6,39 @@ const findThreeLargestNumbers = (array) => {
   const tuple = [-Infinity];
   let arrayNum;
   let tupleNum;
+  let nextTupleNum;
   for (let i = 0; i < array.length; i += 1) {
     arrayNum = array[i];
     if (arrayNum > tuple[0]) {
       // iterate across tuple array, call each iterated item tupleNum
-      for (let j = 0; j < tuple; j += 1) {
+      // keep track of nextTupleNum as well
+      for (let j = 0; j <= tuple.length; j += 1) {
         tupleNum = tuple[j];
-        // if arrayNum is less than the tupleNum,
-        if (arrayNum < tupleNum) {
-          //splice the arrayNum into the tuple array
-          tuple.splice(j, 0, arrayNum);
-          //if tuple array length is greater than 3:
-          if (tuple.length > 3) {
-            //shift the tuplearray to delete the smallest item.
-            tuple.shift()
-          }
+        nextTupleNum = tuple[j + 1]
+        // if the arrayNum is >= arrayNum and the nextTupleNum doesn't exist:
+        if (arrayNum >= tupleNum && nextTupleNum === undefined) {
+          //splice the arrayNum into the tuple array just beyond our current position
+          tuple.splice(j + 1, 0, arrayNum);
+          //stop inserting into tuple
+          break;
         }
+        else if (arrayNum >= tupleNum && arrayNum <= nextTupleNum) {
+          tuple.splice(j + 1, 0, arrayNum)
+          break;
+        }
+      }
+      // trim if necessary
+      if (tuple.length > 3) {
+        tuple.shift()
       }
     }
   }
-  return tuple;
+  // filter for -Infinity values in tuple
+  return tuple.filter((item) => item !== -Infinity);
 }
 
-console.log(findThreeLargestNumbers([5,1,2,4,3]))
+// console.log(findThreeLargestNumbers([5,1,2,4,3]))
+console.log(findThreeLargestNumbers([15,5,9,10,12]))
+// console.log(findThreeLargestNumbers([141, 1, 17, -7, -17, -27, 18, 541, 8, 7, 7]))
 
 module.exports = { findThreeLargestNumbers }
