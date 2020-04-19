b96d2dabfb5a470261a1a99444cafe450a3aabb7
Jake Wija <jakewija@icloud.com[core]>
2020-04-19 15:25:37 -0700

    ADD tests for less than or equal to condition, (in a positive slope and negative slope)

--- aExpert/medium/monotonicArray/monotonicArray.test.js
+++ aExpert/medium/monotonicArray/monotonicArray.test.js
@@ -4,12 +4,14 @@ const { monotonicArray } = require('./monotonicArray');
 describe('base case', () => {
   it('1', () => {
     expect(monotonicArray([1,2,3,4])).toBe(true)
+    expect(monotonicArray([3,2,1])).toBe(true)
+    expect(monotonicArray([300, 200, 100])).toBe(true)
+    expect(monotonicArray([1,1,2,3,4,5,5,5,6,7,8,8,9,10,11])).toBe(true)
   })
   it('2', () => {
     expect(monotonicArray([1,2,5,4])).toBe(false)
-  })
-  it('3', () => {
-
+    expect(monotonicArray([300, -1, 3, 1000])).toBe(false)
+    expect(monotonicArray([-1,-1,-2,-3,-4,-5,-5,-5,-6,-7,-8,-8,-9,-10,-11])).toBe(false)
   })
 })
 
