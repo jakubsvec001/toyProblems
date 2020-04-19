2bfb3d34151bcf0d72818853e1fc3562caad9872
Jake Wija <jakewija@icloud.com[core]>
2020-04-19 12:31:59 -0700

    FINISH problem in O(n) time, O(n) space

--- aExpert/easy/branchSums/branchSums.js
+++ aExpert/easy/branchSums/branchSums.js
@@ -3,18 +3,42 @@
  * A branch sum is the sum of all values in a Binary Tree branch. A Binary Tree branch is a path of nodes in a tree that starts at the root node and ends at the leaf node.
  */
 
-function binaryTree (value) {
-  this.val = value;
+function BinaryTree(value) {
+  this.value = value;
   this.left = null;
   this.right = null;
 }
 
-binaryTree.prototype.branchSums = function() {
-  console.log(this.val)
-}
+BinaryTree.prototype.branchSums = function () {
+  // create result array
+  const result = [];
+  // define recursive traversal (depth first search, leftward tendancy, O(n))
+  const traverse = (node, currentTotal = 0) => {
+    currentTotal += node.value;
+    if (!node.left && !node.right) {
+      result.push(currentTotal)
+    } else {
+      [node.left, node.right].forEach((child) => {
+      if (child) traverse(child, currentTotal);
+      else result.push(currentTotal);
+      });
+    }
+  };
+  traverse(this);
+  return result;
+};
 
-const bt = new binaryTree(3)
+const test1 = new BinaryTree(1);
+test1.left = new BinaryTree(2);
+test1.right = new BinaryTree(3);
+test1.left.left = new BinaryTree(4);
+test1.left.right = new BinaryTree(5);
+test1.right.left = new BinaryTree(6);
+test1.right.right = new BinaryTree(7);
+test1.left.left.left = new BinaryTree(8);
+test1.left.left.right = new BinaryTree(9);
+test1.left.right.left = new BinaryTree(10);
 
-console.log(bt.branchSums())
+console.log(test1.branchSums())
 
-module.exports = { binaryTree };
+module.exports = { BinaryTree };
