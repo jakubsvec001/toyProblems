616bdc3bd793386d0d5826af683d45695e992e28
Jake Wija <jakewija@icloud.com[core]>
2020-04-19 12:52:13 -0700

    ADD test for defining a new branch to avoid 'summing a branch' that is not actually a branch

--- aExpert/easy/branchSums/branchSums.test.js
+++ aExpert/easy/branchSums/branchSums.test.js
@@ -23,9 +23,31 @@ test1.left.right.left = new BinaryTree(10);
 
 const test2 = new BinaryTree(1)
 
+const test3 = new BinaryTree(0);
+test3.left = new BinaryTree(9);
+test3.right = new BinaryTree(1);
+test3.right.left = new BinaryTree(15);
+test3.right.right = new BinaryTree(10);
+test3.right.right.left = new BinaryTree(100);
+test3.right.right.right = new BinaryTree(200)
+
+const test4 = new BinaryTree(0);
+test4.right = new BinaryTree(1)
+test4.right.right = new BinaryTree(10)
+test4.right.right.right = new BinaryTree(100)
+
+
 describe('base cases', () => {
   test('0.1', () => {
     expect(test1.branchSums()).toEqual([ 15, 16, 18, 8, 10, 11 ])
+  })
+
+  test('0.2', () => {
+    expect(test3.branchSums()).toEqual([ 9, 16, 111, 211 ])
+  })
+
+  it('0.3', () => {
+    expect(test4.branchSums()).toEqual()
   })
 })
 
