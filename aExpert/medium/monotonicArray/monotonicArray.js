cd5854ccde42b23dfce9a7f63bce74bba06bc3e2
Jake Wija <jakewija@icloud.com[core]>
2020-04-19 16:41:33 -0700

    COMPLETE problem

--- aExpert/medium/monotonicArray/monotonicArray.js
+++ aExpert/medium/monotonicArray/monotonicArray.js
@@ -4,19 +4,29 @@ given an array, return a boolean for its monotinicicity
 
 const monotonicArray = (inputArray) => {
   // if inputArray length <= two && inputArray length > 0, return true
-
+  if (inputArray.length <= 2) return true;
   // use first two points to define positive or negative slope
-  let initialSlope = inputArray[0] < inputArray[1]; // true = positive, false = negative
-  let currentSlope;
+  let increasing = true,
+      decreasing = true,
+      currentSlope;
   for (let a = 1; a < inputArray.length; a += 1) {
-    currentSlope = inputArray[a - 1] < inputArray[a];
-    if (initialSlope !== currentSlope) {
-      return false
+    // if comparison is the same, continue
+    if (inputArray[a] !== inputArray[a-1]) {
+      currentSlope = inputArray[a] < inputArray[a-1];
+      if (currentSlope) {
+        decreasing = false
+        if (!decreasing && !increasing) return false;
+      } else {
+        increasing = false;
+        if (!decreasing && !increasing) return false;
+      }
     }
   }
   return true;
 }
 
 console.log(monotonicArray([1,2,5,4]))
+console.log(monotonicArray([300, -1, 3, 1000]))
+console.log(monotonicArray([-1,-1,-2,-3,-4,-5,-5,-5,-6,-7,-8,-8,-9,-10,-11]))
 
 module.exports = { monotonicArray };
--- aExpert/medium/monotonicArray/monotonicArray.test.js
+++ aExpert/medium/monotonicArray/monotonicArray.test.js
@@ -3,15 +3,25 @@ const { monotonicArray } = require('./monotonicArray');
 
 describe('base case', () => {
   it('1', () => {
-    expect(monotonicArray([1,2,3,4])).toBe(true)
-    expect(monotonicArray([3,2,1])).toBe(true)
-    expect(monotonicArray([300, 200, 100])).toBe(true)
-    expect(monotonicArray([1,1,2,3,4,5,5,5,6,7,8,8,9,10,11])).toBe(true)
+    expect(monotonicArray([1,2,3,4])).toBe(true)    
   })
   it('2', () => {
+    expect(monotonicArray([1,1,2,3,4,5,5,5,6,7,8,8,9,10,11])).toBe(true)
+  })
+  it('4', () => {
+    expect(monotonicArray([3,2,1])).toBe(true)
+  })
+  it('5', () => {
+    expect(monotonicArray([300, 200, 100])).toBe(true)
+  })
+  it('6', () => {
     expect(monotonicArray([1,2,5,4])).toBe(false)
+  })
+  it('7', () => {
     expect(monotonicArray([300, -1, 3, 1000])).toBe(false)
-    expect(monotonicArray([-1,-1,-2,-3,-4,-5,-5,-5,-6,-7,-8,-8,-9,-10,-11])).toBe(false)
+  })
+  it('8', () => {
+    expect(monotonicArray([-1,-1,-2,-3,-4,-5,-5,-5,-6,-7,-8,-8,-9,-10,-11])).toBe(true)
   })
 })
 
@@ -20,6 +30,9 @@ describe('edge case', () => {
     expect(monotonicArray([1,2])).toBe(true)
   })
   it('inputArray length less than 3, 1 input', () => {
-    expect(monotonicArray([1])).toBe(false)
+    expect(monotonicArray([1])).toBe(true)
+  })
+  it('inputArray length less than 3, 0 input', () => {
+    expect(monotonicArray([])).toBe(true)
   })
 })
