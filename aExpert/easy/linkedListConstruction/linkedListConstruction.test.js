9bbb853a2edc5ac9ceaad8bb2b409f3190295a23
Jake Wija <jakewija@icloud.com[core]>
2020-04-19 20:13:28 -0700

    REFACTOR TESTS for setHead() and setTail()

--- aExpert/easy/linkedListConstruction/linkedListConstruction.test.js
+++ aExpert/easy/linkedListConstruction/linkedListConstruction.test.js
@@ -1,27 +1,43 @@
 const { DoublyLinkedList, Node } = require('./linkedListConstruction');
 
-let DLL;
 
-beforeEach(() => {
-  DLL = new DoublyLinkedList()
-  DLL.setHead(new Node(1))
-  DLL.setTail(new Node(2))
-});
+const DLL1 = new DoublyLinkedList();
+DLL1.setHead(new Node(1));
+DLL1.setHead(new Node(2));
+DLL1.setHead(new Node(3));
+
+const DLL2 = new DoublyLinkedList();
+DLL2.setTail(new Node(1));
+DLL2.setTail(new Node(2));
+DLL2.setTail(new Node(3));
+
 
 describe('base case', () => {
   describe('setHead()', () => {
-    it('sets head', () => {
-      expect(DLL.head.value).toBe(1)
+    it('1', () => {
+      expect(DLL1.convertToArray()[0]).toEqual([3,2,1])
+      expect(DLL1.convertToArray()[1]).toEqual([1,2,3])
+    })
+    it('2', () => {
+      expect(DLL1.head.value).toBe(3)
     })
   })
   describe('setTail()', () => {
-    it('sets tail', () => {
-      expect(DLL.tail.value).toBe(2)
+    it('1', () => {
+      expect(DLL2.convertToArray()[0]).toEqual([1,2,3])
+      expect(DLL2.convertToArray()[1]).toEqual([3,2,1])
+    })
+    it('2', () => {
+      expect(DLL2.tail.value).toBe(3)
     })
   })
   describe('insertBefore()', () => {
-    it('', () => {
-      
+    DLL = new DoublyLinkedList()
+    let n0 = DLL.setHead(new Node(1))
+    let n1 = DLL.setTail(new Node(2))
+    DLL.insertBefore(n1, new Node(1000))
+    it('inserts in between 2 nodes', () => {
+      expect(DLL.head.next.value).toBe(1000)
     })
   })
   describe('insertAfter()', () => {
