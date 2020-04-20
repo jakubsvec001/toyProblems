3376a67e999ac28b0b343db6843fb60ad60e2df1
Jake Wija <jakewija@icloud.com[core]>
2020-04-20 13:41:29 -0700

    ADD tests for insertBefore() and insertAfter()

--- aExpert/easy/linkedListConstruction/linkedListConstruction.test.js
+++ aExpert/easy/linkedListConstruction/linkedListConstruction.test.js
@@ -1,5 +1,6 @@
 const { DoublyLinkedList, Node } = require('./linkedListConstruction');
 
+let expected;
 
 const DLL1 = new DoublyLinkedList();
 DLL1.setHead(new Node(1));
@@ -11,101 +12,93 @@ DLL2.setTail(new Node(1));
 DLL2.setTail(new Node(2));
 DLL2.setTail(new Node(3));
 
+const DLL3 = new DoublyLinkedList();
+let n0 = DLL3.setHead(new Node(1));
+let n1 = DLL3.setTail(new Node(2));
+DLL3.insertBefore(n1, new Node(1000));
 
 describe('base case', () => {
   describe('setHead()', () => {
     it('1', () => {
-      expect(DLL1.convertToArray().foreward).toEqual([3,2,1])
-      expect(DLL1.convertToArray().backward).toEqual([1,2,3])
-    })
+      expect(DLL1.convertToArray().foreward).toEqual([3, 2, 1]);
+      expect(DLL1.convertToArray().backward).toEqual([1, 2, 3]);
+    });
     it('2', () => {
-      expect(DLL1.head.value).toBe(3)
-    })
-  })
+      expect(DLL1.head.value).toBe(3);
+    });
+  });
   describe('setTail()', () => {
     it('1', () => {
-      expect(DLL2.convertToArray().foreward).toEqual([1,2,3])
-      expect(DLL2.convertToArray().backward).toEqual([3,2,1])
-    })
+      expect(DLL2.convertToArray().foreward).toEqual([1, 2, 3]);
+      expect(DLL2.convertToArray().backward).toEqual([3, 2, 1]);
+    });
     it('2', () => {
-      expect(DLL2.tail.value).toBe(3)
-    })
-  })
+      expect(DLL2.tail.value).toBe(3);
+    });
+  });
   describe('insertBefore()', () => {
-    DLL = new DoublyLinkedList()
-    let n0 = DLL.setHead(new Node(1))
-    let n1 = DLL.setTail(new Node(2))
-    DLL.insertBefore(n1, new Node(1000))
     it('inserts in between 2 nodes', () => {
-      expect(DLL.head.next.value).toBe(1000)
-    })
-  })
+      expected = DLL3.convertToArray();
+      expect(expected.forward).toEqual([1, 1000, 2]);
+      expect(expected.backward).toEqual([2, 1000, 1]);
+    });
+    it('inserts into head if desired', () => {
+      DLL3.insertBefore(n0, new Node(99));
+      expected = DLL3.convertToArray();
+      expect(expected.forward).toEqual([99, 1, 1000, 2]);
+      expect(expected.backward).toEqual([2, 1000, 1, 99]);
+      expect(DLL3.head.value).toBe(99);
+    });
+  });
   describe('insertAfter()', () => {
-    it('', () => {
-      
-    })
-  })
+    it('inserts into the middle of multiple nodes', () => {
+      DLL3.insertAfter(n0, new Node(2));
+      expected = DLL3.convertToArray();
+      expect(expected.forward).toEqual([99, 1, 5, 1000, 2]);
+    });
+    it('inserts into the tail, if desired', () => {
+      DLL3.insertAfter(n1, new Node(555));
+      expected = DLL3.convertToArray();
+      expect(expected.forward).toEqual([99, 1, 5, 1000, 2, 555]);
+    });
+  });
   describe('insertAtPosition()', () => {
-    it('', () => {
-      
-    })
-  })
+    it('', () => {});
+  });
   describe('removeNodesWithValue()', () => {
-    it('', () => {
-      
-    })
-  })
+    it('', () => {});
+  });
   describe('remove()', () => {
-    it('', () => {
-      
-    })
-  })
+    it('', () => {});
+  });
   describe('containsNodeWithValue()', () => {
-    it('', () => {
-      
-    })
-  })
-})
+    it('', () => {});
+  });
+});
 
 describe('edge case', () => {
   describe('setHead()', () => {
-    it('', () => {
-      
-    })
-  })
+    it('', () => {});
+  });
   describe('setTail()', () => {
-    it('', () => {
-      
-    })
-  })
+    it('', () => {});
+  });
   describe('insertBefore()', () => {
-    it('', () => {
-      
-    })
-  })
+    it('', () => {});
+  });
   describe('insertAfter()', () => {
-    it('', () => {
-      
-    })
-  })
+    it('', () => {});
+  });
   describe('insertAtPosition()', () => {
-    it('', () => {
-      
-    })
-  })
+    it('', () => {});
+  });
   describe('removeNodesWithValue()', () => {
-    it('', () => {
-      
-    })
-  })
+    it('', () => {});
+  });
   describe('remove()', () => {
-    it('', () => {
-      
-    })
-  })
+    it('', () => {});
+  });
   describe('containsNodeWithValue()', () => {
-    it('', () => {
-      
-    })
-  })
-})
+    it('', () => {});
+  });
+});
