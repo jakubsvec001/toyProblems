0d6464fb2b9c849ed73846e9c2ceb1a34f39c248
Jake Wija <jakewija@icloud.com[core]>
2020-04-22 15:07:27 -0700

    ADD specs for ._getChildren() and ._getParent() methods, with edge cases

--- aExpert/medium/minHeapConstruction/minHeapConstruction.test.js
+++ aExpert/medium/minHeapConstruction/minHeapConstruction.test.js
@@ -2,7 +2,8 @@ const { MinHeap } = require('./minHeapConstruction');
 
 describe('buildHeap', ()=>{
   it('#1', () => {
-
+    const h1 = new MinHeap([10,9,8,7,6,5,3,2,1])
+    expect(h1.heap).toEqual([1,2,4,3,6,5,8,10,7,9])
   })
 })
 
@@ -35,3 +36,15 @@ describe('insert', ()=>{
 
   })
 })
+
+describe('_getChildren(), _getParent()', () => {
+  it('#1', () => {
+    const h2 = new MinHeap([1,2,3,4,5]);
+    expect(h2._getChildren(0).left).toEqual({"index": 1, "value": 2});
+    expect(h2._getChildren(-1)).toEqual({"index": undefined, "value": undefined})
+    expect(h2._getParent(3)).toEqual({"index": 1, "value": 2})
+    expect(h2._getParent(4)).toEqual({"index": 1, "value": 2})
+    expect(h2._getParent(5)).toEqual({"index": undefined, "value": undefined})
+    expect(h2._getParent(-1)).toEqual({"index": undefined, "value": undefined})
+  })
+});
