5fc57cb2fa99093c1f8a8db0578f8d85a9f72163
Jake Wija <jakewija@icloud.com[core]>
2020-04-22 16:21:45 -0700

    RENAME variables that have inputs that are indexes.

--- aExpert/medium/minHeapConstruction/minHeapConstruction.js
+++ aExpert/medium/minHeapConstruction/minHeapConstruction.js
@@ -4,7 +4,7 @@
  * childTwo = 2i + 2;
  * parent = math.floor((i-1)/2)
  * in a min heap, parents are always smaller than children
- * in a min heap when sifting down and the parent is larger than either child, swap parent with smallest child;  
+ * in a min heap when sifting down and the parent is larger than either child, swap parent with smallest child;
  */
 class MinHeap {
   constructor(array) {
@@ -14,10 +14,19 @@ class MinHeap {
   buildHeap(array) {
     // find the last parent
     this.heap = array;
-    let lastParent = this._getParent(this.heap.length - 1);
+    let lastParent = this._getParentOfIndex(this.heap.length - 1);
+    let left;
+    let right;
+    let minChild;
+    while (lastParent.value) {
+      // get children objects
+      minChild = this._getMinChildOfIndex(lastParent.index);
+      // if (left.value < right.value)
+      lastParent = this._getParentOfIndex(lastParent.index);
+    }
     return this.heap;
   }
-  
+
   //use with insertion and deletion
   siftDown() {}
 
@@ -33,35 +42,60 @@ class MinHeap {
   //insert integer into heap. Insert at the end, then sift up
   insert(value) {}
 
-  _swap(i1, i2){
+  _swap(i1, i2) {
     const heapLength = this.heap.length;
-    if ((i1 < 0 || i1 > heapLength-1) || (i2 < 0 || i2 > heapLength-1)) return this;
-    [this.heap[i2], this.heap[i1]] = [this.heap[i1], this.heap[i2]]
+    if (i1 < 0 || i1 > heapLength - 1 || i2 < 0 || i2 > heapLength - 1)
+      return this;
+    [this.heap[i2], this.heap[i1]] = [this.heap[i1], this.heap[i2]];
     return this;
   }
-  
-  _getChildren(i){
-    if (i > this.heap.length-1 || i < 0) return {'index': undefined, 'value': undefined};
+
+  _getChildrenOfIndex(i) {
+    if (i > this.heap.length - 1 || i < 0)
+      return { index: undefined, value: undefined };
     const left = {},
           right = {};
-    left.index = (2*i) + 1;
+    left.index = 2 * i + 1;
     left.value = this.heap[left.index];
-    right.index = (2*i) + 2;
+    right.index = 2 * i + 2;
     right.value = this.heap[right.index];
     return { left, right };
   }
-  _getParent(i){
-    if (i > this.heap.length-1 || i < 0) return {'index': undefined, 'value': undefined};
+  _getParentOfIndex(i) {
+    if (i > this.heap.length - 1 || i < 0)
+      return { index: undefined, value: undefined };
     const parent = {};
-    parent.index = Math.floor((i-1)/2)
+    parent.index = Math.floor((i - 1) / 2);
     if (parent.index > -1) {
-      parent.value = this.heap[parent.index] 
-      return parent
+      parent.value = this.heap[parent.index];
+      return parent;
     }
     parent.value = undefined;
     return parent;
   }
+
+  _getMinChildOfIndex(parentIndex) {
+    const { left, right } = this._getChildrenOfIndex(parentIndex);
+    let minChild;
+    // if both children present
+    if (left.value && right.value) {
+      minChild = [left, right].reduce(
+        (agg, next) => {
+          if (agg.value < next.value) return agg;
+          else return next;
+        },
+        { value: Infinity },
+      );
+      return minChild;
+    } else if (left.value) {
+      return left;
+    }
+    return null;
+  }
 }
 
+const h1 = new MinHeap([10, 9, 8, 7, 6, 5, 4, 3, 2, 1]);
+
+console.log(h1._getMinChildOfIndex(4));
 
 module.exports = { MinHeap };
--- aExpert/medium/minHeapConstruction/minHeapConstruction.test.js
+++ aExpert/medium/minHeapConstruction/minHeapConstruction.test.js
@@ -2,7 +2,7 @@ const { MinHeap } = require('./minHeapConstruction');
 
 describe('buildHeap', ()=>{
   it('#1', () => {
-    const h1 = new MinHeap([10,9,8,7,6,5,3,2,1])
+    const h1 = new MinHeap([10,9,8,7,6,5,4,3,2,1])
     expect(h1.heap).toEqual([1,2,4,3,6,5,8,10,7,9])
   })
 })
@@ -46,14 +46,14 @@ describe('_swap()', ()=> {
   })
 })
 
-describe('_getChildren(), _getParent()', () => {
+describe.only('_getChildrenOfIndex(), _getParentOfIndex()', () => {
   it('#1', () => {
     const h2 = new MinHeap([1,2,3,4,5]);
-    expect(h2._getChildren(0).left).toEqual({"index": 1, "value": 2});
-    expect(h2._getChildren(-1)).toEqual({"index": undefined, "value": undefined})
-    expect(h2._getParent(3)).toEqual({"index": 1, "value": 2})
-    expect(h2._getParent(4)).toEqual({"index": 1, "value": 2})
-    expect(h2._getParent(5)).toEqual({"index": undefined, "value": undefined})
-    expect(h2._getParent(-1)).toEqual({"index": undefined, "value": undefined})
+    expect(h2._getChildrenOfIndex(0).left).toEqual({"index": 1, "value": 2});
+    expect(h2._getChildrenOfIndex(-1)).toEqual({"index": undefined, "value": undefined})
+    expect(h2._getParentOfIndex(3)).toEqual({"index": 1, "value": 2})
+    expect(h2._getParentOfIndex(4)).toEqual({"index": 1, "value": 2})
+    expect(h2._getParentOfIndex(5)).toEqual({"index": undefined, "value": undefined})
+    expect(h2._getParentOfIndex(-1)).toEqual({"index": undefined, "value": undefined})
   })
 });
