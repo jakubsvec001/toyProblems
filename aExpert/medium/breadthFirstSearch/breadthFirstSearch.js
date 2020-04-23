44a50c3466f400d41a9db93c9ce111261f081250
Jake Wija <jakewija@icloud.com[core]>
2020-04-23 11:57:35 -0700

    CLEAN code, sync

--- aExpert/medium/breadthFirstSearch/breadthFirstSearch.js
+++ aExpert/medium/breadthFirstSearch/breadthFirstSearch.js
@@ -41,12 +41,4 @@ class Node {
   }
 }
 
-
-const tree20 = new Node(0).addChild(1).addChild(2).addChild(3);
-tree20.children[0].addChild(4).addChild(5)
-tree20.children[2].addChild(6).addChild(7);
-tree20.children[0].children[1].addChild(8).addChild(9)
-tree20.children[2].children[1].addChild(10)
-console.log(tree20.breadthFirstSearchPure([])); //.toEqual([0,1,2,3,4,5,6,7,8,9,10]);
-
 module.exports = { Node };
