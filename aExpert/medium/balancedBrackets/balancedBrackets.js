f0ac71ab153fc988ec391650944bd1825d6f1ee5
Jake Wija <jakewija@icloud.com[core]>
2020-04-23 12:12:51 -0700

    ADD outline comments for function

--- aExpert/medium/balancedBrackets/balancedBrackets.js
+++ aExpert/medium/balancedBrackets/balancedBrackets.js
@@ -1,6 +1,10 @@
 
-const balancedBrackets = () => {
-  return
+const balancedBrackets = (str) => {
+  // initialize a stack
+  const stack = [];
+  // iterate string array and add bracket if open ['(', '[', '{'] 
+  // or remove if closed [')',']','}']
+  // if the last item of the stack does not match the open closing bracket, return false
 };
 
 module.exports = { balancedBrackets };
