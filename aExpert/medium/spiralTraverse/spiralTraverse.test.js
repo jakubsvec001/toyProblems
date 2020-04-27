60a3e8583a7beaaf18560301425e7f59a6a55140
Jake Wija <jakewija@icloud.com[core]>
2020-04-27 15:53:47 -0700

    ADD test for dissimilar m and n lengths for array input

--- aExpert/medium/spiralTraverse/spiralTraverse.test.js
+++ aExpert/medium/spiralTraverse/spiralTraverse.test.js
@@ -1,12 +1,26 @@
-const { spiralTraverse } = require('./spiralTraverse')
+const { spiralTraverse } = require('./spiralTraverse');
 
 let array;
 let result;
 
 describe('base case tests', () => {
-  it('1', ()=>{
-    array = [[1,2,3,4],[12,13,14,5], [11, 16, 15, 6], [10,9,8,7]]
-    result = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
-    expect(spiralTraverse(array)).toEqual(result)
+  it('1', () => {
+    array = [
+      [1, 2, 3, 4],
+      [12, 13, 14, 5],
+      [11, 16, 15, 6],
+      [10, 9, 8, 7],
+    ];
+    result = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
+    expect(spiralTraverse(array)).toEqual(result);
+  });
+  it('2', () => {
+    array = [
+      [1, 2, 3, 4, 5],
+      [14, 15, 16, 17, 6],
+      [13, 20, 19, 18, 7],
+      [12, 11, 10, 9, 8],
+    ];
+    result = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
   })
-})
+});
