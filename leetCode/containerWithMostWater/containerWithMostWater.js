dda3bae1a2a6a3004358d781622ab866cf830597
Jake Wija <jakewija@icloud.com[core]>
2020-05-07 12:20:40 -0700

    FINSIH prompt

--- leetCode/containerWithMostWater/containerWithMostWater.js
+++ leetCode/containerWithMostWater/containerWithMostWater.js
@@ -1,7 +1,37 @@
 
+/**
+ * Given n non-negative integers a1, a2, ..., an , where each represents a point at coordinate (i, ai). n vertical lines are drawn such that the two endpoints of line i is at (i, ai) and (i, 0). Find two lines, which together with x-axis forms a container, such that the container contains the most water.
 
-const containerWithMostWater = () => {
+Note: You may not slant the container and n is at least 2.
 
+Input: [1,8,6,2,5,4,8,3,7]
+Output: 49
+ */
+const getArea = (array, L, R) => {
+  const smaller = Math.min(array[L], array[R])
+  const length = R - L;
+  return smaller * length;
 }
 
-export default containerWithMostWater;
+const containerWithMostWater = (array) => {
+  let biggest = 0;
+  let L = 0;
+  let R = array.length - 1;
+  let area;
+  while (L < R) {
+    area = getArea(array, L, R)
+    if (area > biggest) {
+      biggest = area;
+    }
+    if (array[L] > array[R]) {
+      R -= 1;
+    } else {
+      L += 1;
+    }
+  }
+  return biggest;
+}
+
+console.log(containerWithMostWater([1,8,6,2,5,4,8,3,7]))
+module.exports = { containerWithMostWater, getArea };
+
--- leetCode/containerWithMostWater/containerWithMostWater.test.js
+++ leetCode/containerWithMostWater/containerWithMostWater.test.js
@@ -1,11 +1,16 @@
-const containerWithMostWater = require('./containerWithMostWater');
+const { containerWithMostWater, getArea } = require('./containerWithMostWater');
 
 describe('base cases', () => {
   it('1', () => {
-
+    const test1 = {
+      Input: [1,8,6,2,5,4,8,3,7],
+      Output: 49,
+    }
+    expect(containerWithMostWater(test1.Input)).toBe(test1.Output);
   });
-  it('2', () => {
-
+  it('2 getArea() gets correct area', () => {
+    expect(getArea([1,8,6,2,5,4,8,3,7], 0, 8)).toBe(8)
+    expect(getArea([1,8,6,2,5,4,8,3,7], 1, 8)).toBe(49)
   });
   it('3', () => {
 
