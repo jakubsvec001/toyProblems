062fb880b80a28e386efa6297e52718a2a931194
Jake Wija <jakewija@icloud.com[core]>
2020-06-09 10:48:56 -0700

    CREATE directory, test file, program file

--- aExpert/medium/longestPeak/longestPeak.test.js
+++ aExpert/medium/longestPeak/longestPeak.test.js
@@ -1,3 +1,8 @@
 const { longestPeak } = require('./longestPeak');
 
-console.log(longestPeak([1, 2, 3]));
+describe('base cases', () => {
+  const array_0 = [1, 2, 3, 3, 4, 0, 10, 6, 5, -1, -3, 2, 3];
+  it('0', () => {
+    expect(longestPeak(array_0)).toBe(6);
+  });
+});
