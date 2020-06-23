70a343c937afab5f609c72916e9621785b87a05e
Jakub Svec <jakubsvec001@gmail.com>
2020-06-23 15:02:53 -0700

    UPDATE base case tests with single-node tree input

--- aExpert/easy/nodeDepths/nodeDepths.test.js
+++ aExpert/easy/nodeDepths/nodeDepths.test.js
@@ -3,18 +3,22 @@ const { BinaryTree, nodeDepths } = require('./nodeDepths');
 console.log(BinaryTree)
 
 describe('base case', () => {
-  const node1 = new BinaryTree(1);
-  node1.left = new BinaryTree(2);
-  node1.right = new BinaryTree(3);
-  node1.left.left = new BinaryTree(4);
-  node1.left.right = new BinaryTree(5);
-  node1.left.left.left = new BinaryTree(8);
-  node1.left.left.right = new BinaryTree(9);
-  node1.right.left = new BinaryTree(6);
-  node1.right.right = new BinaryTree(7);
+  const tree1 = new BinaryTree(1);
+  tree1.left = new BinaryTree(2);
+  tree1.right = new BinaryTree(3);
+  tree1.left.left = new BinaryTree(4);
+  tree1.left.right = new BinaryTree(5);
+  tree1.left.left.left = new BinaryTree(8);
+  tree1.left.left.right = new BinaryTree(9);
+  tree1.right.left = new BinaryTree(6);
+  tree1.right.right = new BinaryTree(7);
 
   it('simple tree', () => {
-    expect(nodeDepths(node1)).toBe(16);
+    expect(nodeDepths(tree1)).toBe(16);
+  });
+  it('only root node in tree', ()=> {
+    const tree2 = new BinaryTree(1);
+    expect(nodeDepths(tree2)).toBe(0)
   })
 })
 
