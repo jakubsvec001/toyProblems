a37a5bcd65d4c8d1d7c894e27b1d9271a5f5b15b
Jakub Svec <jakubsvec001@gmail.com>
2020-06-29 16:47:52 -0700

    UPDATE

--- aExpert/easy/nthFibonacci/nthFibonacci.test.ts
+++ aExpert/easy/nthFibonacci/nthFibonacci.test.ts
@@ -1,17 +1,18 @@
-const { memoizedGetNthFib } = require('./nthFibonacci');
+// const { memoizedGetNthFib } = require('./nthFibonacci');
+import {memoizedGetNthFib} from './nthFibonacci';
 
+jest.setTimeout(5000);
 describe('base case', () => {
   it('1', () => {
     expect(memoizedGetNthFib(0)).toBe(0);
     expect(memoizedGetNthFib(1)).toBe(0);
     expect(memoizedGetNthFib(2)).toBe(1);
   });
-  // it("doesn't time out", (done) => {
+  // it("doesn't time out", () => {
   //   try {
-  //     expect(MemoizedGetNthFib(1000)).toBe(218922995834555200000);
-  //     done();
+  //     expect(memoizedGetNthFib(100)).toBe(218922995834555200000);
   //   } catch (error) {
-  //     done(error)
+  //     console.log(error)
   //   }
-  // })
+  // }, 1000)
 });
