2258db6144f06a9d60891e8e12bb99f10b52efaf
Jakub Svec <jakubsvec001@gmail.com>
2020-07-22 15:01:52 -0700

    ADD test case to diskStacking test description

--- aExpert/medium/groupAnagrams/diskStacking/diskStacking.test.ts
+++ aExpert/medium/groupAnagrams/diskStacking/diskStacking.test.ts
@@ -3,6 +3,9 @@ import diskStacking, { isLarger, totalHeight } from './diskStacking';
 describe('diskStacking', () => {
   it('1', () => {
     expect(diskStacking([[2,1,2], [3,2,3], [2,2,8], [2,3,4], [1,3,1], [4,4,5]])).toEqual([[2,1,2], [3,2,3], [4,4,5]])
+  });
+  it('2', () => {
+    expect(diskStacking([[2,1,2], [3,2,3], [2,2,8], [2,3,4], [1,8,1], [4,4,5]])).toEqual([[1,8,1]])
   })
 })
 
