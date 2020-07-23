b98800b30961e48eb4f897defd8bc1ddc5fec0c7
Jakub Svec <jakubsvec001@gmail.com>
2020-07-23 11:05:06 -0700

    CLEAN commit

--- aExpert/medium/groupAnagrams/diskStacking/diskStacking.ts
+++ aExpert/medium/groupAnagrams/diskStacking/diskStacking.ts
@@ -56,4 +56,41 @@ const diskStacking = (inputArray:number[][]):number[][] => {
   return maxStack;
 }
 
+
+const diskStackingDynamic = (disks:number[][]) => {
+  disks.sort((a,b)=> a[2] - b[2]);
+  const heights = disks.map(disk => disk[2]);
+  const sequences = new Array(disks.length);
+  let maxHeightIdx = 0;
+  for (let leadIdx = 1; leadIdx < disks.length; leadIdx += 1) {
+    const leadDisk = disks[leadIdx];
+    for (let trailIdx = 0; trailIdx < leadIdx; trailIdx += 1) {
+      const trailDisk = disks[trailIdx];
+      if (areValidDimensions(trailDisk, leadDisk)) {
+        if (heights[leadIdx] <= leadDisk[2] + heights[trailIdx]) {
+          heights[leadIdx] = leadDisk[2] + heights[trailIdx];
+          sequences[leadIdx] = trailIdx;
+        }
+      }
+    }
+    if (heights[leadIdx] >= heights[maxHeightIdx]) maxHeightIdx = leadIdx;
+  }
+  return buildSequence(disks, sequences, maxHeightIdx)
+}
+
+function areValidDimensions(a, b) {
+  return a[0] < b[0] && a[1] < b[1] && a[2] < b[2];
+}
+
+function buildSequence(arr, seqs, currIdx) {
+  const seq = [];
+  while (currIdx !== undefined) {
+    seq.unshift(arr[currIdx]);
+    currIdx = seqs[currIdx];
+  }
+  return seq;
+}
+
+console.log(diskStackingDynamic([[2,1,2], [3,2,3], [2,2,8], [2,3,4], [1,3,1], [4,4,5]]))
+
 export default diskStacking;
