1aa7b621ccd510d22f9255ba8ba2def893e8af82
Jakub Svec <jakubsvec001@gmail.com>
2020-07-27 13:23:06 -0700

    FINISH findLoop with typescript

--- aExpert/hard/findLoop/findLoop.ts
+++ aExpert/hard/findLoop/findLoop.ts
@@ -1,18 +1,17 @@
-import { loopTest0, loopTest1, noLoopTest } from './tests'
 import LL from './LinkedListClass';
 
 
 const findLoop = (head:LL):LL => {
-  let slow:LL = head.next;
-  let fast:LL = head.next.next;
+  let slow:LL = head.next!;
+  let fast:LL = head.next!.next!;
   while (slow !== fast) {
-    slow = slow.next;
-    fast = fast.next.next;
+    slow = slow.next!;
+    fast = fast.next!.next!;
   }
   slow = head;
   while (slow !== fast) {
-    slow.next;
-    fast.next;
+    slow = slow.next;
+    fast = fast.next;
   }
   return slow;
 }
